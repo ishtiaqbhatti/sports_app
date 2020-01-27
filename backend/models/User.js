cdca189cd426d4c87a13e807fb7de8b8f706b086
Ishtiaq Bhatti <Ishtiaqbhatti455@gmail.com>
2020-01-27 14:09:35 -0500

    Routes, Contoller added

---
+++ backend/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  userName: { type: String, required: [true, 'Please add a username'] },
+  email: {
+    type: String,
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please add a valid email'
+    ]
+  },
+  password: {
+    type: String,
+    required: [true, 'Please add a password'],
+    minlength: 6
+  },
+  role: { type: String, enum: ['admin', 'manager', 'coach', 'athelete'] },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('User', userSchema);
